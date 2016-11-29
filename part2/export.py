6fb795982fa99396765f4065658a7931fc9d0f00
janos-kakuszi <jan.kakuszi@gmail.com>
2016-11-29 23:15:38 +0100

    Add files via upload

--- part2/export.py
+++ part2/export.py
@@ -1,2 +1,12 @@
+from reports import *
 
-# Export functions
+def export_results(result):
+    file =open("result.txt", "a+")
+    file.write(str(result) + "\n")
+
+export_results(str(get_most_played("game_stat.txt")))
+export_results(str(sum_sold("game_stat.txt")))
+export_results(str(get_selling_avg("game_stat.txt")))
+export_results(str(count_longest_title("game_stat.txt")))
+export_results(str(get_date_avg("game_stat.txt")))
+export_results(str(get_game("game_stat.txt", "Diablo II")))
--- part2/printing.py
+++ part2/printing.py
@@ -1,2 +1,11 @@
+from reports import *
 
-# Printing functions
+def main():
+    print(get_most_played("game_stat.txt"))
+    print(sum_sold("game_stat.txt"))
+    print(count_longest_title("game_stat.txt"))
+    print(get_selling_avg("game_stat.txt"))
+    print(get_date_avg("game_stat.txt"))
+    print(get_game("game_stat.txt", "Counter-Strike"))
+
+main()
